7eea87e22a73c186878539a7cf23ce3c413e9ce9
Elisha <elisham.balfour@gmail.com>
2023-10-03 17:56:36 +0100

    JS coding practice 1 commit

---
+++ index.js
@@ -0,0 +1,58 @@
+let colour = 'green';
+let age = 25;
+
+
+console.log(colour);
+console.log(age);
+
+age = 28;
+console.log(age);
+
+
+function subtract (num1,num2){
+    console.log(num1 - num2);
+}
+
+subtract(5, 2);
+
+function divide (num1, num2){
+    console.log(num1 / num2);
+}
+
+divide(10, 2);
+
+function concatenate(str1, str2) {
+    var concatenatedString = str1 + str2;
+    return concatenatedString;
+}
+var string1 = "hello, ";
+var string2 = "me";
+var result = concatenate(string1, string2);
+console.log(result);
+
+
+let carName = 'Kia'
+console.log(carName);
+
+function function3() {
+    console.log('Kia');
+ }
+ 
+ function3();
+
+multiply = function(y, x) {
+    return y * x;   
+}
+var y = "3";
+var x = "5";
+console.log(y * x);
+
+let sky = 'blue';
+        
+function function2() {
+    console.log(sky);
+    console.log('hello my name is Elisha');
+}
+
+function2();
+
